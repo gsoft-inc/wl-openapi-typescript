3319abb6f4cdcd9ffacfa28263e9e3786d3184d3
Mathieu Gamache <mathieu.gamache@workleap.com>
2024-06-04 17:13:23 -0400

    Some update

--- packages/create-schemas/src/bin.ts
+++ packages/create-schemas/src/bin.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import openapiTS, { astToString } from "openapi-typescript";
+import ts, { InterfaceDeclaration, PropertySignature } from "typescript";
 
 console.log("Received command: ", process.argv);
 
@@ -12,9 +13,37 @@ console.log(`openApiPath: ${openApiPath}`);
 console.log(`outputPath: ${outputPath}`);
 
 
-// TODO: Support filename and URL
-const ast = await openapiTS(new URL(openApiPath, import.meta.filename));
-const contents = astToString(ast);
+// 1. Create a TypeScript AST from the OpenAPI schema
+const ast = await openapiTS(new URL(openApiPath));
 
-// (optional) write to file
-fs.writeFileSync(outputPath, contents);
+// 2. Find the node where all the DTOs are defined, and extract their names
+// const componentsNode = ast.find((node) => ts.isInterfaceDeclaration(node) && node.name.escapedText === "components") as InterfaceDeclaration;
+//console.log(ts.ClassificationType);
+// const schemaNode = componentsNode.members.find((node) => ts.isPropertySignature(node) && ts.isIdentifier(node.name) && node.name.escapedText === "schemas") as PropertySignature;
+const schemaNames: string[] = [];
+// const typeNode = schemaNode.type;
+// if (typeNode && ts.isTypeNode(typeNode)) {
+//   typeNode.forEachChild((node) => {
+//     if (ts.isPropertySignature(node)) {
+//       const nameNode = node.name;
+//       if (ts.isIdentifier(nameNode)) {
+//         schemaNames.push(nameNode.escapedText as string);
+//       }
+//     }
+//   });
+// }
+
+
+// 3. Convert the AST to a string
+let contents = astToString(ast);
+
+// 4. Add the `export type X = components["schemas"]["X"]` lines
+for (const schemaName of schemaNames) {
+    contents +=
+      `export type ${schemaName} = components["schemas"]["${schemaName}"];\n`;
+}
+
+
+// 5. Write the content to a file (TODO: Validate USER experience)
+fs.mkdirSync("./dist", { recursive: true });
+fs.writeFileSync("./dist/schema.ts", contents);
