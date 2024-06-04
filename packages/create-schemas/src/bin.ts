8d72440adb5fb99c12c9ef4b04c7717e082a1a35
Mathieu Gamache <mathieu.gamache@workleap.com>
2024-06-04 16:28:10 -0400

    Using the openapi-typescript library

--- packages/create-schemas/src/bin.ts
+++ packages/create-schemas/src/bin.ts
@@ -1,5 +1,20 @@
-console.log("Hello World!");
+import fs from "node:fs";
+import openapiTS, { astToString } from "openapi-typescript";
+
+console.log("Received command: ", process.argv);
 
 // Access command-line arguments
 const args = process.argv.slice(2);
-console.log("Received arguments:", args);
+
+const openApiPath = args[0] ?? "C:/dev/idp/wl-openapi-typescript/wl-openapi-typescript/v1.yaml";
+const outputPath = args[1] ?? "C:/dev/idp/wl-openapi-typescript/wl-openapi-typescript/output.ts";
+console.log(`openApiPath: ${openApiPath}`);
+console.log(`outputPath: ${outputPath}`);
+
+
+// TODO: Support filename and URL
+const ast = await openapiTS(new URL(openApiPath, import.meta.filename));
+const contents = astToString(ast);
+
+// (optional) write to file
+fs.writeFileSync(outputPath, contents);
