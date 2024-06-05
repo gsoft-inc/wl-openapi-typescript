986fc2c7e77845ed84d08b4397150eb99baf69d5
Mathieu Gamache <math.gamache@gmail.com>
2024-06-05 17:04:04 -0400

    [idp-1615] Generate typescript schema (#2)
    * Using the openapi-typescript library
    
    * Some update
    
    * Update script to re-export types
    
    * Foward openapi-typescript flags
    
    * Publish preview for testing
    
    * Set comparaison base
    
    * Cleanup
    
    * Refactor to make it more testable
    
    * Set version to 0.1.0
    
    * Cleanup
    
    * PR fixes
    
    * Fix version
    
    * Update lock
    
    ---------
    
    Co-authored-by: Mathieu Gamache <mathieu.gamache@workleap.com>

--- packages/create-schemas/src/bin.ts
+++ packages/create-schemas/src/bin.ts
@@ -1,5 +1,28 @@
-console.log("Hello World!");
+import fs from "node:fs";
+import path from "path";
+import { getOpenApiTsOptionForArgs, getOutputPath } from "./argsHelper.ts";
+import { generateSchemas } from "./openapiTypescriptHelper.ts";
+
+console.log("Received command: ", process.argv);
 
 // Access command-line arguments
 const args = process.argv.slice(2);
-console.log("Received arguments:", args);
+
+const openApiTsOptions = getOpenApiTsOptionForArgs(args);
+
+const openApiPath = args[0];
+const outputPath = getOutputPath(args);
+
+if (!openApiPath || !outputPath) {
+    throw new Error("Both openApiPath and outputPath must be provided");
+}
+
+console.log("Starting OpenAPI TypeScript types generation...");
+console.log(`\t-openApiPath: ${openApiPath}`);
+console.log(`\t-outputPath: ${outputPath}`);
+
+const contents = await generateSchemas(openApiPath, outputPath, openApiTsOptions);
+
+// Write the content to a file
+fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+fs.writeFileSync(outputPath, contents);
