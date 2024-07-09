2f4042f229ad8cb6fb9460daec20a197d9a7b2e4
Tommy Josépovic <tommy.josepovic@gsoft.com>
2024-07-09 16:00:04 -0400

    unflake

--- packages/create-schemas/tests/watch.test.ts
+++ packages/create-schemas/tests/watch.test.ts
@@ -5,26 +5,31 @@ import { join } from "node:path";
 import { watch } from "../src/watch.ts";
 
 describe("watch", () => {
-    test("changing the input", async ({ expect, onTestFinished }) => {
-        // Setup
-        const tempFolder = await createTemporaryFolder({ onTestFinished });
+    test(
+        "changing the input",
+        // Filesystem timing can be pretty flaky
+        { retry: 5 },
+        async ({ expect, onTestFinished }) => {
+            // Setup
+            const tempFolder = await createTemporaryFolder({ onTestFinished });
 
-        const input = join(tempFolder, "input.json");
-        const output = join(tempFolder, "output.ts");
+            const input = join(tempFolder, "input.json");
+            const output = join(tempFolder, "output.ts");
 
-        const watcher = await watch({ input, output });
-        onTestFinished(() => watcher.stop());
+            const watcher = await watch({ input, output });
+            onTestFinished(() => watcher.stop());
 
-        // 1st output
-        await copyFile(join(dataFolder, "petstore.json"), input);
-        await new Promise(resolve => watcher.once("done", resolve));
-        let result = await readFile(output, "utf-8");
-        expect(result).toMatchSnapshot();
+            // 1st output
+            await copyFile(join(dataFolder, "petstore.json"), input);
+            await new Promise((resolve) => watcher.once("done", resolve));
+            let result = await readFile(output, "utf-8");
+            expect(result).toMatchSnapshot();
 
-        // 2nd output
-        await copyFile(join(dataFolder, "todo.json"), input);
-        await new Promise(resolve => watcher.once("done", resolve));
-        result = await readFile(output, "utf-8");
-        expect(result).toMatchSnapshot();
-    });
+            // 2nd output
+            await copyFile(join(dataFolder, "todo.json"), input);
+            await new Promise((resolve) => watcher.once("done", resolve));
+            result = await readFile(output, "utf-8");
+            expect(result).toMatchSnapshot();
+        }
+    );
 });
